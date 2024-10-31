95a3be7fa709185a3e080f058a372c7f182706f9
DuckDood <b.c.youtub@gmail.com>
2024-10-31 16:58:14 +1300

    listind directories /w scroll

--- main.cpp
+++ main.cpp
@@ -6,15 +6,50 @@
 namespace fs = std::filesystem;
 int main() {
 	initscr();
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
 	int ys, xs;
+	int getched;
 	std::string username = getenv("USER");
 	std::string path = "/home/"+username;
+	char cpath[100];
 	getmaxyx(stdscr, ys, xs);
 	refresh();
+	int i = 0;
+	int strt =  0;
+	while (true) {
+	i = 0;
+	clear();
 	for (const auto & entry : fs::directory_iterator(path)){
+	if (i >= strt){
         printw(entry.path().string().c_str());
 	printw("\n");
-	refresh();}
-	getch();
+	}
+	i++;
+	if(i >= ys + strt) {
+	break;
+	}
+	}
+	getched = getch();
+	if((char)getched == 'w') {
+		strt--;
+	} else if((char) getched == 's') {
+	strt++;
+	} else if((char) getched == 'c') {
+		echo();
+		scanw(cpath);
+		path = cpath;
+		noecho();
+	}
+
+	if(strt < 0) {
+		strt =0;
+
+	}
+	else if(strt > ys - (ys-i)-1) {
+		strt = ys - (ys-i)-1;
+	}
+	}
 	endwin();
 }
